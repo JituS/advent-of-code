eface99399658f59193be5ca6e79bf8da3df513c
Jitendra <Jitendrs@thoughtworks.com>
2019-12-08 16:05:39 +0530

    Fourth problem (Day two closed)

--- src/main/scala/Computer.scala
+++ src/main/scala/Computer.scala
@@ -3,25 +3,36 @@ import scala.io.Source
 
 object Computer {
   def main(args: Array[String]): Unit = {
-    @tailrec def operation(i: Int, array: Array[Int]) {
-      if (i < array.length) {
-        val opCode = array(i)
+    @tailrec def operation(instructionPointer: Int, inputValues: Array[Int]) {
+      if (instructionPointer < inputValues.length) {
+        val opCode = inputValues(instructionPointer)
         if (opCode == 99) return
-        val operand1 = array(i + 1)
-        val operand2 = array(i + 2)
-        array(array(i + 3)) = opCode match {
-          case 1 => array(operand1) + array(operand2)
-          case 2 => array(operand1) * array(operand2)
+        val operand1 = inputValues(instructionPointer + 1)
+        val operand2 = inputValues(instructionPointer + 2)
+        inputValues(inputValues(instructionPointer + 3)) = opCode match {
+          case 1 => inputValues(operand1) + inputValues(operand2)
+          case 2 => inputValues(operand1) * inputValues(operand2)
         }
-        operation(i + 4, array)
+        operation(instructionPointer + 4, inputValues)
       }
     }
 
     val input = readFileAsLines("computer_input.txt").next().split(",").map(_.toInt)
-    input(1) = 12
-    input(2) = 2
-    operation(0, input)
-    println(input(0))
+    val operableInput = input.clone
+    var outerLoopCounter = 0
+    var innerLoopCounter = 0
+    while (outerLoopCounter <= 99 && operableInput(0) != 19690720) {
+      innerLoopCounter = 0
+      while (innerLoopCounter <= 99 && operableInput(0) != 19690720) {
+        input.copyToArray(operableInput)
+        operableInput(1) = outerLoopCounter
+        operableInput(2) = innerLoopCounter
+        operation(0, operableInput)
+        innerLoopCounter = innerLoopCounter + 1
+      }
+      outerLoopCounter = outerLoopCounter + 1
+    }
+    println(innerLoopCounter-1, outerLoopCounter-1)
   }
 
   private def readFileAsLines(fileName: String) = {

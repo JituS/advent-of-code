f51a58eeebe37a32625bbc948585257e8ebbd90c
Jitendra <Jitendrs@thoughtworks.com>
2019-12-10 11:42:51 +0530

    day 5 first problem

--- src/main/scala/Computer.scala
+++ src/main/scala/Computer.scala
@@ -2,37 +2,49 @@ import scala.annotation.tailrec
 import scala.io.Source
 
 object Computer {
+
+  def getIndex(opcode: Int): Int = {
+    opcode match {
+      case 3 => 1
+      case 4 => 1
+      case 104 => 1
+      case _ => 3
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    @tailrec def operation(instructionPointer: Int, inputValues: Array[Int]) {
-      if (instructionPointer < inputValues.length) {
-        val opCode = inputValues(instructionPointer)
-        if (opCode == 99) return
-        val operand1 = inputValues(instructionPointer + 1)
-        val operand2 = inputValues(instructionPointer + 2)
-        inputValues(inputValues(instructionPointer + 3)) = opCode match {
-          case 1 => inputValues(operand1) + inputValues(operand2)
-          case 2 => inputValues(operand1) * inputValues(operand2)
-        }
-        operation(instructionPointer + 4, inputValues)
+    @tailrec def operation(i: Int, array: Array[Int]) {
+      if (array(i) == 4) {
+        println(array(array(i + 1)))
+      }else if(array(i) == 104){
+        println(array(i + 1))
       }
+      else {
+        val i1 = array(i + getIndex(array(i)))
+        array(i1) = array(i) match {
+          case 1 => array(array(i + 1)) + array(array(i + 2))
+          case 2 => array(array(i + 1)) * array(array(i + 2))
+          case 3 => readInt
+          case _ =>
+            val operationToPerform = array(i).toString.takeRight(2).toInt
+            val modes = array(i).toString.dropRight(2).split("").reverse.padTo(3, "0").reverse
+            modes match {
+              case Array(a, b, c) =>
+                val value1 = if (c == "0") array(array(i + 1)) else array(i + 1)
+                val value2 = if (b == "0") array(array(i + 2)) else array(i + 2)
+                println()
+                operationToPerform match {
+                  case 1 => value1 + value2
+                  case 2 => value1 * value2
+                }
+            }
+        }
+      }
+      operation(i + getIndex(array(i)) + 1, array)
     }
 
     val input = readFileAsLines("computer_input.txt").next().split(",").map(_.toInt)
-    val operableInput = input.clone
-    var outerLoopCounter = 0
-    var innerLoopCounter = 0
-    while (outerLoopCounter <= 99 && operableInput(0) != 19690720) {
-      innerLoopCounter = 0
-      while (innerLoopCounter <= 99 && operableInput(0) != 19690720) {
-        input.copyToArray(operableInput)
-        operableInput(1) = outerLoopCounter
-        operableInput(2) = innerLoopCounter
-        operation(0, operableInput)
-        innerLoopCounter = innerLoopCounter + 1
-      }
-      outerLoopCounter = outerLoopCounter + 1
-    }
-    println(innerLoopCounter-1, outerLoopCounter-1)
+    operation(0, input)
   }
 
   private def readFileAsLines(fileName: String) = {

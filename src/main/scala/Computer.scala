0e7d7ab5654be8617d34fb238cef2a71eb91061b
Jitendra <Jitendrs@thoughtworks.com>
2019-12-11 21:43:04 +0530

    Refactored day 5 problem

--- src/main/scala/Computer.scala
+++ src/main/scala/Computer.scala
@@ -1,54 +1,51 @@
 import scala.annotation.tailrec
-import scala.io.Source
+import scala.io.{Source, StdIn}
 
 object Computer {
 
-  def getIndex(opcode: Int): Int = {
+  def getIndex(opcode: String): Int = {
     opcode match {
-      case 3 => 1
-      case 4 => 1
-      case 104 => 1
+      case a if a.endsWith("4") || a.endsWith("3") => 1
       case _ => 3
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    @tailrec def operation(i: Int, array: Array[Int]) {
-      if (array(i) == 4) {
-        println(array(array(i + 1)))
-      }else if(array(i) == 104){
-        println(array(i + 1))
-      }
-      else {
-        val i1 = array(i + getIndex(array(i)))
-        array(i1) = array(i) match {
-          case 1 => array(array(i + 1)) + array(array(i + 2))
-          case 2 => array(array(i + 1)) * array(array(i + 2))
-          case 3 => readInt
-          case _ =>
-            val operationToPerform = array(i).toString.takeRight(2).toInt
-            val modes = array(i).toString.dropRight(2).split("").reverse.padTo(3, "0").reverse
-            modes match {
-              case Array(a, b, c) =>
-                val value1 = if (c == "0") array(array(i + 1)) else array(i + 1)
-                val value2 = if (b == "0") array(array(i + 2)) else array(i + 2)
-                println()
-                operationToPerform match {
-                  case 1 => value1 + value2
-                  case 2 => value1 * value2
-                }
-            }
-        }
-      }
-      operation(i + getIndex(array(i)) + 1, array)
+  private def handleParameterMode(i: Int, inputArray: Array[String]) = {
+    val operation = inputArray(i).split("").reverse.padTo(5, "0").reverse
+    val operand = operation.takeRight(2).mkString
+    val modes: Array[String] = operation.dropRight(2)
+    modes match {
+      case Array(a, b, c) =>
+        val value1 = if (c == "0") inputArray(inputArray(i + 1).toInt) else inputArray(i + 1)
+        val value2 = if (b == "0" && !operand.endsWith("4")) inputArray(inputArray(i + 2).toInt) else inputArray(i + 2)
+        calculate(operand, value1, value2)
     }
+  }
 
-    val input = readFileAsLines("computer_input.txt").next().split(",").map(_.toInt)
-    operation(0, input)
+  def calculate(operation: String, val1: String, val2: String): String = {
+    (operation match {
+      case "01" => val1.toInt + val2.toInt
+      case "02" => val1.toInt * val2.toInt
+      case "03" => StdIn.readLine
+      case "04" =>
+        println(val1)
+        val1
+    }).toString
   }
 
   private def readFileAsLines(fileName: String) = {
     val bufferedSource = Source.fromInputStream(getClass.getResourceAsStream(fileName))
     for (line <- bufferedSource.getLines()) yield line
+  }
+
+  @tailrec def processInput(currentIndex: Int, inputArray: Array[String]) {
+    val outputIndex = inputArray(currentIndex + getIndex(inputArray(currentIndex))).toInt
+    inputArray(outputIndex) = handleParameterMode(currentIndex, inputArray)
+    processInput(currentIndex + getIndex(inputArray(currentIndex)) + 1, inputArray)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = readFileAsLines("computer_input.txt").next().split(",")
+    processInput(0, input)
   }
 }

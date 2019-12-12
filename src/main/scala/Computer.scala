72a7c06b1bef2e62b084756599bae163e1562f10
Jitendra <Jitendrs@thoughtworks.com>
2019-12-13 00:27:38 +0530

    WIP

--- src/main/scala/Computer.scala
+++ src/main/scala/Computer.scala
@@ -3,6 +3,10 @@ import scala.io.Source
 
 object Computer {
 
+  val PAUSED = "paused"
+  val HALTED = "halted"
+  val RUNNING = "running"
+
   def getIndex(opcode: String): Int = {
     opcode match {
       case a if List("4", "3").exists(a.endsWith) => 1
@@ -11,36 +15,43 @@ object Computer {
     }
   }
 
+  case class Amplifier(calculatedValue: Option[String], outputIndex: Int, generatedOutput: Iterator[Int], state: String)
+
   private def handleParameterMode(i: Int, inputArray: Array[String], inputs: Iterator[Int]) = {
+    println(i)
+    println(inputArray.mkString(","))
     val operation = inputArray(i).split("").reverse.padTo(5, "0").reverse
     val operand = operation.takeRight(2).mkString
-    val modes: Array[String] = operation.dropRight(2)
-    modes match {
+    operation.dropRight(2) match {
       case Array(a, b, c) =>
         operand match {
           case "01" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (Some((value1.toInt + value2.toInt).toString), i + getIndex(operand), "")
+            Amplifier(Some((value1.toInt + value2.toInt).toString), i + getIndex(operand), inputs, RUNNING)
           case "02" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (Some((value1.toInt * value2.toInt).toString), i + getIndex(operand), "")
+            Amplifier(Some((value1.toInt * value2.toInt).toString), i + getIndex(operand), inputs, RUNNING)
           case "03" =>
-            (Some(inputs.next().toString), i + getIndex(operand), "")
+            Amplifier(Some(inputs.next().toString), i + getIndex(operand), inputs, RUNNING)
           case "04" =>
             val value1 = if (c == "0") inputArray(inputArray(i + 1).toInt) else inputArray(i + 1)
-            (None, i + getIndex(operand), value1)
+            Amplifier(None, i + getIndex(operand), inputs ++ Iterator(value1.toInt), RUNNING)
           case "05" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (None, if (value1.toInt != 0) value2.toInt - 1 else i + getIndex(operand), "")
+            Amplifier(None, if (value1.toInt != 0) value2.toInt - 1 else i + getIndex(operand), inputs, RUNNING)
           case "06" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (None, if (value1.toInt == 0) value2.toInt - 1 else i + getIndex(operand), "")
+            Amplifier(None, if (value1.toInt == 0) value2.toInt - 1 else i + getIndex(operand), inputs, RUNNING)
           case "07" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (Some((if (value1 < value2) 1 else 0).toString), i + getIndex(operand), "")
+            Amplifier(Some((if (value1 < value2) 1 else 0).toString), i + getIndex(operand), inputs, RUNNING)
           case "08" =>
             val (value1, value2) = getValues(i, inputArray, b, c)
-            (Some((if (value1 == value2) 1 else 0).toString), i + getIndex(operand), "")
+            Amplifier(Some((if (value1 == value2) 1 else 0).toString), i + getIndex(operand), inputs, RUNNING)
+          case "99" =>
+            println(i, "halted")
+            println(inputArray.mkString(","))
+            InstructionResult(None, 0, inputs, state = true)
         }
     }
   }
@@ -51,24 +62,39 @@ object Computer {
     (value1.toInt, value2.toInt)
   }
 
-  private def readFileAsLines(fileName: String) = {
-    val bufferedSource = Source.fromInputStream(getClass.getResourceAsStream(fileName))
+  private def readFileAsLines() = {
+    val bufferedSource = Source.fromInputStream(getClass.getResourceAsStream("computer_input.txt"))
     for (line <- bufferedSource.getLines()) yield line
   }
 
-  @tailrec def processInput(currentIndex: Int, inputArray: Array[String], inputs: Iterator[Int]): String = {
-    val tuple = handleParameterMode(currentIndex, inputArray, inputs)
-    if (!tuple._3.isEmpty) return tuple._3
-    tuple._1.foreach(inputArray(inputArray(tuple._2).toInt) = _)
-    processInput(tuple._2 + 1, inputArray, inputs)
+  @tailrec def processInput(currentIndex: Int, inputArray: Array[String], inputs: Iterator[Int]): Amplifier = {
+    val instructionResult = handleParameterMode(currentIndex, inputArray, inputs)
+    if (instructionResult.state) return instructionResult
+    instructionResult.calculatedValue.foreach(inputArray(inputArray(instructionResult.outputIndex).toInt) = _)
+    processInput(instructionResult.outputIndex + 1, inputArray, inputs ++ instructionResult.generatedOutput)
   }
 
   def main(args: Array[String]): Unit = {
-    val input = readFileAsLines("computer_input.txt").next().split(",")
-    println((0 to 4).permutations.map { e =>
-      e.foldLeft(0) { (a, b) =>
-        processInput(0, input, Iterator(b, a)).toInt
-      }
-    }.max)
+    val input = readFileAsLines().next().split(",")
+    //    println((0 to 4).permutations.map(feedbackLoop(_, input)._1).max)
+    feedbackLoop(Array(9, 8, 7, 6, 5), input)._1.foreach(println)
+  }
+
+  def feedbackLoop(signal: Seq[Int], input: Array[String], finalOutput: (Iterator[Int], Boolean) = (Iterator(0), false)): (Iterator[Int], Boolean) = {
+    //    if (false) return finalOutput
+    signal.map((_, input)).foldLeft(0){ (initialInput: Int, amplifier: (Int, Array[String])) =>
+      processInput()
+    }
+
+//    signal.map().foldLeft(finalOutput) { (a, b) =>
+//      val output: InstructionResult = processInput(0, input, Iterator(b) ++ a._1)
+//      val c = output.generatedOutput.toList
+//      (c.toIterator, false)
+      //      println(output)
+      //      if (output.isHalted) return (output.generatedOutput, false)
+      //      else (output.generatedOutput, false)
+//    }
+    //    println("signal, input.mkString(\",\"), output -> " + signal, input.mkString(","), output)
+    //    feedbackLoop(signal, input, output)
   }
 }
